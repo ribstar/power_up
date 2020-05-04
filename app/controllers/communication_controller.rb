4a29cdfee1494429b4f1dce0c057bc0d59d77225
ribstar <wood.sk.love315@icloud.com>
2020-05-04 12:38:01 +0900

    first

---
+++ app/controllers/communication_controller.rb
@@ -0,0 +1,2 @@
+class CommunicationController < ApplicationController
+end
---
+++ test/controllers/communication_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommunicationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
